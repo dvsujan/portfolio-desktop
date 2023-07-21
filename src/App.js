097b07e5bb688d7df14775a5456c819c7ab20a3d
dvsujan <dvsujan@gmail.com>
2023-07-21 14:05:51 +0530

    portfolioInit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { isMobile, isBrowser } from "react-device-detect";
 function App() {
+  const Home = () => {
+    if (isMobile) {
+      return <>this is home mobile view</>;
+    } else {
+      return <>this is Home in browser</>;
+    }
+  };
+
+  const Projects = () => {
+if (isMobile) {
+      return <>this is projects  mobile view</>;
+
+    } else {
+      return <>this is projects in browser</>;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </div>
   );
 }
